a84a7ad0fcf4179f62776b52305e082385c52ddf
Yan MATAGNE <yan.matagne@gmail.com>
2016-06-26 15:12:40 +0200

    feat(screenshot,reporter) : Add schreenshot and reporter option in npm command

--- bin/tasty.js
+++ bin/tasty.js
@@ -3,16 +3,19 @@
 var program = require('commander');
 var fs = require("fs");
 var core = require("tasty-core");
+var Reporter = require("tasty-core/app/tasty-reporter");
 
 program
     .arguments('<tasty script file>')
     .option('-i, --instructions <instruction files>', 'Custom instruction files separated with semicolons')
     .option('-p, --parameters <parameter files>', 'Custom parameter files separated with semicolons')
     .option('-b, --browser <browser>', 'Browser in which to execute script (either firefox, chrome, phantomJs, ... depending on your drivers)')
+    .option('-s, --screenshotpath <screenshotpath>', 'Screenshotpath is path where screenshot is saved')
+    .option('-r, --reporter <reporter>', 'Select the desired report between : junit')
     .action(function (file) {
 
-        console.log('instructions: %s parameters: %s file: %s',
-            program.instructions, program.parameters, file);
+        console.log('instructions: %s parameters: %s file: %s screenshotpath: %s reporter: %s',
+            program.instructions, program.parameters, file, program.screenshotpath, program.reporter);
 
         if (program.instructions) {
             program.instructions.split(";").forEach(function (filePath) {
@@ -28,14 +31,23 @@ program
         if (program.browser) {
             browser = program.browser;
         }
+        var screenshotpath = './tasty-report/screenshot'
+        if (program.screenshotpath) {
+            screenshotpath = program.screenshotpath;
+        }
+        var reporter = './tasty-report/'
+        if (program.reporter) {
+            reporter = program.reporter;
+        }
 
         fs.readFile(file, "utf8", function (err, data) {
             if (!err) {
-                core.init(browser);
+                core.init(browser, screenshotpath);
 
                 core.execute(data).then(function (instructions) {
-                    for (var i = 0; i < instructions.length; i++) {
-                        console.log(instructions[i].toString())
+                    switch (reporter) {
+                        case "junit": Reporter.generateJunitReporter(instructions); break;
+                        default: Reporter.generateConsoleLog(instructions);
                     }
                 });;
                 core.stop();
